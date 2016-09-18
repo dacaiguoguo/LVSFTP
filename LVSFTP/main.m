2b79eeaffae03ecdd69a1b5b4b291e0359f80c53
dacaiguoguo <dacaiguoguo@gmail.com>
2016-09-18 19:59:53 +0800

    Initial Commit

---
+++ LVSFTP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LVSFTP
+//
+//  Created by yanguo sun on 18/09/2016.
+//  Copyright © 2016 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
