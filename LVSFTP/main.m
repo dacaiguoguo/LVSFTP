0b7bd6ea0cd9750113f35e5dff60e5be749965ac
dacaiguoguo <dacaiguoguo@gmail.com>
2016-09-18 20:18:52 +0800

    add sftp demo

--- LVSFTP/main.m
+++ LVSFTP/main.m
@@ -7,11 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NMSSH.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         NSLog(@"Hello, World!");
+
+        
+        
+        NMSSHSession *session = [[NMSSHSession alloc] initWithHost:@"192.168.0.1:22" andUsername:@"admin"];
+        [session connect];
+        
+        if (session.isConnected) {
+            [session authenticateByPassword:@"admin"];
+            
+            if (session.isAuthorized) {
+                [session.sftp connect];
+                
+                NSArray *remoteFileList = [session.sftp contentsOfDirectoryAtPath:@"/path/ios/"];
+                for (NMSFTPFile *file in remoteFileList) {
+                    NSLog(@"%@", file.filename);
+                }
+                NSString *content = [[NSString alloc] initWithData:[session.sftp contentsAtPath:@"/path/ios/js.txt"] encoding:NSUTF8StringEncoding];
+                NSLog(@"%@",content);
+                BOOL writeResult = [session.sftp writeContents:[@"abc11" dataUsingEncoding:NSUTF8StringEncoding] toFileAtPath:@"/path/ios/js.txt"];
+                assert(writeResult);
+            }
+            
+            [session disconnect];
+        }
+        NSRunLoop *currLoop = [NSRunLoop currentRunLoop];
+        [currLoop addPort:[NSPort port] forMode:NSDefaultRunLoopMode];
+        [currLoop run];
     }
     return 0;
 }
